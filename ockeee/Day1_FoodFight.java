4d1dbc51b5246f288acee04b9cf93c21109b0754
Ockeee <167044265+Ockeee@users.noreply.github.com>
2024-10-25 00:15:08 +0900

    Update Day1_FoodFight.java

--- ockeee/Day1_FoodFight.java
+++ ockeee/Day1_FoodFight.java
@@ -1,21 +1,18 @@
 public class Day1_FoodFight {
-    public String solution(int[] food) {
-        // 순 + 0 + 역순 이렇게 만들면 되겠다 생각하였음
-        
-        // 가운데는 항상 0이기 때문에
-        String answer = "0";
-        
-        // 앞인 순 먼저
-        for(int i = 1; i<food.length; i++) {
-            // 2로 나눠지지 않는 애들은 정리
-            int halfLength = food[i] / 2;
-            // 역순 순회
-            for (int j = 0; j < halfLength; j++) {
-                // 처음 생각했던 순 + 0 + 역순으로 종결
-                answer = i + answer + i;
-            }
-        }
+   public String solution(int[] food) {
 
+       // 물은 항상 0이므로
+        String answer = "0";
+       // 음식의 양이 홀수일 경우 짝수로 정리
+        for(int i = 1; i<food.length; i++) {
+            food[i] = food[i] - food[i] % 2;
+        }
+       // 역순을 순회하는 문자열
+        for(int i = food.length - 1; i >= 1; i--) {
+            // 각 음식의 절반만큼 반복
+            for (int j = 0; j < food[i] / 2; j++)
+                answer = i + answer + i;
+        }
         return answer;
     }
 }
