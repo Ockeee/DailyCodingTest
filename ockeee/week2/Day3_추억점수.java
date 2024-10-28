af110bd39c104945d96ec1453164a625c1f63746
Ockeee <167044265+Ockeee@users.noreply.github.com>
2024-10-28 23:40:19 +0900

    Update Day3_추억점수.java

--- ockeee/week2/Day3_추억점수.java
+++ ockeee/week2/Day3_추억점수.java
@@ -1,9 +1,13 @@
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        // 사진의 총 개수인 배열을 하나 먼저 만들어서
          int[] answer = new int[photo.length];
-        
+
+        // 1) 사진을 하나씩 먼저 확인
         for(int i=0; i<photo.length; i++) {
+            // 2) 사진의 이름을 하나씩 확인
         	for(int j=0; j<photo[i].length; j++) {
+                // 3) 이름을 검사 해서 사진[i],[j]와 이름[k]이 같으면 점수를 더해서 결과 도출
         		for(int k=0; k<name.length; k++) {
         			if(photo[i][j].equals(name[k])) answer[i] += yearning[k];
         		}
