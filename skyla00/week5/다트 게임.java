d8b8b0b623497eb44b3c659bc84a8aac72d5b61f
Skyla <165969200+skyla00@users.noreply.github.com>
2024-11-19 08:54:17 +0900

    Create 다트 게임.java

---
+++ skyla00/week5/다트 게임.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+// dartResult 3분할. arraylist 에 넣어서 인덱스 부여. 
+// 정규표현식 영어나 #, * 있으면 자르기.
+// 점수 부여해줄 arraylist 선언.
+// 1,2,3과 D,S,T 를 보고 점수 부여. power 함수가 있나 
+// 만약 *가 있으면 현재 인덱스와 현재 인덱스 -1 까지 *2 해주기.
+// # 이 있으면 현재 인덱스 * -1 하기. 
